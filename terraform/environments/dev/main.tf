51ded4c2765e7103867f038154e5d4a2430f969a
Fremmerlid <richard.fremmerlid@gov.bc.ca>
2025-06-26 16:27:44 -0700

    updated .tf files

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -54,7 +54,7 @@ module "poc_storage_account" {
 
   # Pass the runner's IP address to the module so it can create a firewall rule.
   allowed_ip_rules = var.allowed_ip_rules
-  service_principal_id = var.dev_service_principal_id
+  service_principal_id = var.service_principal_id
 }
 
 #================================================================================
