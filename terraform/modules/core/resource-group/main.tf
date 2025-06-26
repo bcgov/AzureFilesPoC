5c96c4f9e415bf55c4c7fcbda9f7ca0ffa01eae1
richfrem <richard.fremmerlid@gov.bc.ca>
2025-06-26 12:00:30 -0700

    updated variable names

--- terraform/modules/core/resource-group/main.tf
+++ terraform/modules/core/resource-group/main.tf
@@ -11,21 +11,21 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_role_assignment" "storage_account_contributor" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Storage Account Contributor"
-  principal_id         = var.dev_service_principal_id
+  principal_id         = var.service_principal_id
 }
 
 # Assign Storage Blob Data Contributor for blob/container operations
 resource "azurerm_role_assignment" "blob_data_contributor" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = var.dev_service_principal_id
+  principal_id         = var.service_principal_id
 }
 
 # Assign Storage File Data SMB Share Contributor for Azure Files operations
 resource "azurerm_role_assignment" "file_data_contributor" {
   scope                = azurerm_resource_group.main.id
   role_definition_name = "Storage File Data SMB Share Contributor"
-  principal_id         = var.dev_service_principal_id
+  principal_id         = var.service_principal_id
 }
 
 # Assign custom role for role assignment management (data/control plane automation)
