ff1f04a385c376477d9191a940f86ac7492df9aa
richfrem <richard.fremmerlid@gov.bc.ca>
2025-07-02 17:50:22 -0700

    modified nsg to try to create a subnet as well

--- terraform/modules/bastion/nsg/main.tf
+++ terraform/modules/bastion/nsg/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.12.0"
+    }
+  }
+}
+
 # Bastion NSG Module
 #
 # Assumptions / Constraints:
