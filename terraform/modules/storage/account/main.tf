30d680f28aee3d7199a874cefe22e0193147971d
richfrem <richard.fremmerlid@gov.bc.ca>
2025-07-05 13:32:40 -0700

    updated storage-account main.tf

--- terraform/modules/storage/account/main.tf
+++ terraform/modules/storage/account/main.tf
@@ -9,17 +9,25 @@ terraform {
   }
 }
 
-# This module correctly creates ONLY a private-only Azure Storage Account.
+# This module creates the most minimal, private-only Azure Storage Account possible
+# to ensure compliance with strict Azure Policies that block public endpoints.
 resource "azurerm_storage_account" "main" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.azure_location
   tags                     = var.tags
 
+  # --- Required Arguments for a Standard Account ---
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-  large_file_share_enabled = true
+  account_replication_type = "LRS" # Local-redundant storage
 
-  # This setting is the key to complying with BC Gov policy.
+  # --- CRITICAL SETTING for Policy Compliance ---
+  # This explicitly disables the public network endpoint. This is the only
+  # networking-related argument we will provide to create the cleanest
+  # possible API request for the policy engine.
   public_network_access_enabled = false
+  
+  # NOTE: All other optional arguments like 'large_file_share_enabled',
+  # 'network_rules', etc., have been removed to prevent the provider from
+  # sending any conflicting or unnecessary properties to the Azure API.
 }
