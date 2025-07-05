c2e7f084c3a5064e0f87469916d22d198d0392d0
richfrem <richard.fremmerlid@gov.bc.ca>
2025-07-05 16:10:26 -0700

    updated blob container.tf

--- terraform/modules/storage/blob-container/main.tf
+++ terraform/modules/storage/blob-container/main.tf
@@ -1,5 +1,16 @@
 # Storage Blob Container with least-privilege RBAC
 
+# This block is a best practice for modules to declare their provider requirements.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # This module is now compatible with provider version 3.75.0
+      version = ">= 3.0"
+    }
+  }
+}
+
 resource "azurerm_storage_container" "main" {
   name                  = var.container_name
   storage_account_name  = var.storage_account_name
